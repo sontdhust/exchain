81ca69cfb5442e6cd00f4feb44b9263419f85364
Tran Son (チャン・ソン) <t.son@verve-inc.jp>
2018-06-18 13:20:21 +0900

    [Database]: Add ‘executed’ and ‘updated_at’ to ‘trades’. [Api]: Try to execute trades until it is done.

--- exchain/storage/database.py
+++ exchain/storage/database.py
@@ -70,18 +70,6 @@ def select_assets():
         'ORDER BY assets.id'
     )
     DATABASE['cursor'].execute(query)
-    def load_api(text):
-        """
-        Load api
-        """
-        default_api = {
-            'slack_webhook_url': '',
-            'bitflyer_api_key': '',
-            'bitflyer_api_secret': ''
-        }
-        api = json.loads(text) if text is not None else {}
-        api.update({k: v for k, v in default_api.iteritems() if k not in api})
-        return api
     result = [{
         'id': r[0],
         'ticker_id': r[1],
@@ -127,6 +115,60 @@ def insert_trade(asset_id, side, price, amount, trade_type):
         'created_at': get_current_datetime()
     })
     DATABASE['connector'].commit()
+
+def select_unexecuted_trades():
+    """
+    Select unexecuted trades
+    """
+    query = (
+        'SELECT trades.id, api, exchange, symbol, trades.side, trades.amount, type '
+        'FROM trades '
+        'JOIN assets ON trades.asset_id = assets.id '
+        'JOIN users ON assets.user_id = users.id '
+        'JOIN tickers ON assets.ticker_id = tickers.id '
+        'WHERE executed = 0 AND trades.id IN (SELECT MAX(id) FROM trades GROUP BY asset_id) '
+        'ORDER BY trades.id'
+    )
+    DATABASE['cursor'].execute(query)
+    result = [{
+        'id': r[0],
+        'api': load_api(r[1]),
+        'exchange': r[2],
+        'symbol': r[3],
+        'side': r[4],
+        'amount': r[5],
+        'type': r[6]
+    } for r in DATABASE['cursor'].fetchall()]
+    return result
+
+def update_trade(trade_id):
+    """
+    Update trade
+    """
+    query = (
+        'UPDATE trades '
+        'SET executed = %(executed)s, updated_at = %(updated_at)s '
+        'WHERE id = %(id)s'
+    )
+    DATABASE['cursor'].execute(query, {
+        'id': trade_id,
+        'executed': 1,
+        'updated_at': get_current_datetime()
+    })
+    DATABASE['connector'].commit()
+
+def load_api(text):
+    """
+    Load api
+    """
+    default_api = {
+        'slack_webhook_url': '',
+        'bitflyer_api_key': '',
+        'bitflyer_api_secret': ''
+    }
+    api = json.loads(text) if text is not None else {}
+    api.update({k: v for k, v in default_api.iteritems() if k not in api})
+    return api
 
 def get_current_datetime():
     """
